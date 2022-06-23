c466d987f2e34861b7871bce99abac272464fe0e
Luka <ospopovic@gmail.com>
2022-06-23 16:20:01 +0200

    Added list example and progress bar

---
+++ my-app/src/components/CircularProgressbar.js
@@ -0,0 +1,12 @@
+    import "../App.css";
+    import { ProgressBar } from "react-bootstrap";
+    import React from "react";
+
+    function CircularPBar() {
+        return ( 
+            <ProgressBar value={20}/>
+            
+         );
+    }
+    
+    export default CircularPBar;
--- my-app/src/components/ListOfItems.js
+++ my-app/src/components/ListOfItems.js
@@ -1,13 +1,16 @@
 import { ListGroup } from "react-bootstrap";
 import React from "react";
+import CircularProgressbar from "./CircularProgressbar";
 
 function ListOfItems() {
-    return (  
-        <ListGroup className="mt-4 ">
-            <ListGroup.Item>Example</ListGroup.Item>
-        </ListGroup>
-
-    );
+  return (
+    <ListGroup className="mt-4 ">
+      <ListGroup.Item>
+        <CircularProgressbar />
+        Example
+      </ListGroup.Item>
+    </ListGroup>
+  );
 }
 
 export default ListOfItems;
