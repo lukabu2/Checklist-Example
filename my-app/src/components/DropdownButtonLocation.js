2ba34b4c4a629a573e18364344a3aa69b4931cb3
Luka <ospopovic@gmail.com>
2022-06-28 21:47:35 +0200

    Editing dropdown button and adding content

--- my-app/src/components/DropdownButtonLocation.js
+++ my-app/src/components/DropdownButtonLocation.js
@@ -1,25 +1,71 @@
 import { DropdownButton, Dropdown } from "react-bootstrap";
 import { MdLocationOn } from "react-icons/md";
 import "../App";
-import React from "react";
+import "../App.css";
+import React, { useEffect, useState, Col, Row } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 
 function DropdownButtonLocation() {
+  let [outlet, setOutlet] = useState([{}]);
+  function getAll() {
+    try {
+      axios
+        .get(
+          "http://api-development.synergysuite.net/rest/permission/allowedCompanies?userId=1490106392118050028&corporateId=500000000",
+          {
+            headers: {
+              "synergy-login-token": "bae0c3f2-20a2-4015-b51c-cbc939f4f808",
+            },
+          }
+        )
+        .then((response) => {
+          setOutlet(response.data);
+          console.log(response.data);
+        })
+        .catch((error) => console.log(error));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    getAll();
+  }, []);
+
+  let allOutlets = outlet.map((el, i) => (
+    <Dropdown.Item
+      className="d-flex justify-content-center mt-1"
+      as="button"
+      key={i}
+    >
+      <b>{el.name}</b>
+    </Dropdown.Item>
+  ));
+
   return (
     <div className="d-flex justify-content-center mt-3">
       <DropdownButton
         title={
           <>
-            <MdLocationOn className="mb-1"/>
+            <MdLocationOn className="mb-1" />
             <b>Lokacija</b>
           </>
         }
+        className="CenterDropdownList"
+        // align="end"
         size="sm"
         variant="outline-secondary"
       >
-        <Dropdown.Item as="button"><b>Ulica</b></Dropdown.Item>
-        <Dropdown.Item as="button"><b>Another action</b></Dropdown.Item>
-        <Dropdown.Item as="button"><b>Something else</b></Dropdown.Item>
+        {allOutlets}
+        {/* <Dropdown.Item as="button">
+          <b>Ulica</b>
+        </Dropdown.Item>
+        <Dropdown.Item as="button">
+          <b>Another action</b>
+        </Dropdown.Item>
+        <Dropdown.Item as="button">
+          <b>Something else</b>
+        </Dropdown.Item> */}
       </DropdownButton>
     </div>
   );
--- my-app/src/components/ListOfItems.js
+++ my-app/src/components/ListOfItems.js
@@ -4,38 +4,40 @@ import axios from "axios";
 import CircularProgressbar from "./CircularProgressbar";
 
 function ListOfItems() {
-  let [outlet, setOutlet] = useState([{}]);
-  function getAll() {
-    try {
-      axios
-        .get(
-          "http://api-development.synergysuite.net/rest/permission/allowedCompanies?userId=1490106392118050028&corporateId=500000000",
-          {
-            headers: {
-              "synergy-login-token": "bae0c3f2-20a2-4015-b51c-cbc939f4f808",
-            },
-          }
-        )
-        .then((response) => setOutlet(response.data))
-        .catch((error) => console.log(error));
-    } catch (error) {
-      console.log(error);
-    }
-  }
-  useEffect(() => {
-    getAll();
-  }, []);
+  // let [outlet, setOutlet] = useState([{}]);
+  // function getAll() {
+  //   try {
+  //     axios
+  //       .get(
+  //         "http://api-development.synergysuite.net/rest/permission/allowedCompanies?userId=1490106392118050028&corporateId=500000000",
+  //         {
+  //           headers: {
+  //             "synergy-login-token": "bae0c3f2-20a2-4015-b51c-cbc939f4f808",
+  //           },
+  //         }
+  //       )
+  //       .then((response) => setOutlet(response.data))
+  //       .catch((error) => console.log(error));
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+  // useEffect(() => {
+  //   getAll();
+  // }, []);
 
-  let allOutlets = outlet.map((el,i) => (
-    <ListGroup.Item key={i}>
-      <CircularProgressbar />
-        <p>{el.name}</p>
-        <p>{el.id}</p>
-    </ListGroup.Item>
-  ));
+  // let allOutlets = outlet.map((el,i) => (
+  //   <ListGroup.Item key={i}>
+  //     <CircularProgressbar />
+  //       <p>{el.name}</p>
+  //       <p>{el.id}</p>
+  //   </ListGroup.Item>
+  // ));
   return (
     <ListGroup className="mt-4 ">
-      {allOutlets}
+      <ListGroup.Item>
+       <CircularProgressbar />
+     </ListGroup.Item>
     </ListGroup>
   );
 }
